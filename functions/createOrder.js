9881379e50c6c45cb6173ebb75d9b06e136c62f5
GuilhermeDSantos <53098377+GuilhermeDSantos@users.noreply.github.com>
2024-02-09 09:43:32 -0300

    Update createOrder.js

--- functions/createOrder.js
+++ functions/createOrder.js
@@ -68,7 +68,7 @@ exports = async function(request, response){
     const database = mongodb.db("clients");
 
     // Iniciar a transação
-    const session = database.startSession();
+    const session = database.getMongo().startSession();
     const transactionOptions = {
       readPreference: "primary",
       readConcern: { level: "local" },
