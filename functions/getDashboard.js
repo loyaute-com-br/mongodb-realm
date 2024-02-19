e04cc4fcfc50dc4524d703faeff35303f47e233d
GuilhermeDSantos <53098377+GuilhermeDSantos@users.noreply.github.com>
2024-02-19 09:56:07 -0300

    Update getDashboard.js

--- functions/getDashboard.js
+++ functions/getDashboard.js
@@ -1,47 +1,17 @@
-exports = async function(request, response){
-  try {
-    if (request.body === undefined) {
-      response.setStatusCode(400);
-      response.setBody(JSON.stringify({ "errorType": "MISSING_DATA" }));
-      return;
-    }
+exports = async function() {
+  const collection = context.services.get("mongodb-atlas").db("clients").collection("transactions");
 
-    if (!context.user || !context.user.custom_data.roles.includes("seller")) {
-      response.setStatusCode(401);
-      response.setBody(JSON.stringify({ "errorType": "UNAUTHORIZED_ACCESS" }));
-      return;
-    }
-
-    const body = JSON.parse(await request.body.text());
-
-    if (body.start_date === undefined || body.end_date === undefined) {
-      response.setStatusCode(400);
-      response.setBody(JSON.stringify({ "errorType": "MISSING_DATA" }));
-      return;
-    }
-
-    const mongodb = context.services.get("mongodb-atlas");
-
-    const query = {
-      timestamp: {
-        $gte: body.start_date,
-        $lt: body.end_date
+  const pipeline = [
+    {
+      $group: {
+        _id: null,
+        transactions: { $push: "$$ROOT" },
+        totalRevenue: { $sum: "$value" }
       }
-    };
-
-    const transactions = mongodb.db("clients").collection("transactions")
-        .find({}).toArray();
-
-    let totalRevenue = 0;
-
-    for (let i = 0; i < transactions.length; i++) {
-      totalRevenue += transactions[i].value;
     }
+  ];
 
-    return { revenue: totalRevenue, transactions: transactions };
-    // response.setBody(JSON.stringify({ revenue: totalRevenue, transactions: transactions }));
-  } catch (error) {
-    response.setStatusCode(400);
-    response.setBody(JSON.stringify({ "errorType": "ERROR", "message": error.message }));
-  }
+  const result = await collection.aggregate(pipeline).toArray();
+
+  return result[0];
 };
