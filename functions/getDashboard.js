929ce62ebe56dd3342ff15cdf563490403d78178
GuilhermeDSantos <53098377+GuilhermeDSantos@users.noreply.github.com>
2024-02-19 10:30:01 -0300

    Update getDashboard.js

--- functions/getDashboard.js
+++ functions/getDashboard.js
@@ -59,60 +59,37 @@ exports = async function(request, response){
     }
   ];
 
+  // Pipeline para contar as transações duplicadas por wallet
+  const duplicatedTransactionsPipeline = [
+    {
+      $match: {
+        timestamp: {
+          $gte: new Date(body.start_date),
+          $lt: new Date(body.end_date)
+        }
+      }
+    },
+    {
+      $group: {
+        _id: "$wallet_id",
+        transactions: { $push: "$$ROOT" },
+        count: { $sum: 1 }
+      }
+    },
+    {
+      $match: {
+        count: { $gt: 1 } // Filtra grupos com mais de uma transação
+      }
+    }
+  ];
+
   const transactionsResult = await clientsDB.collection("transactions").aggregate(transactionsPipeline).toArray();
   const walletsResult = await clientsDB.collection("wallets").aggregate(walletsPipeline).toArray();
+  const duplicatedTransactionsResult = await clientsDB.collection("transactions").aggregate(duplicatedTransactionsPipeline).toArray();
 
   return {
     transactions: transactionsResult[0],
-    wallets: walletsResult[0]
+    wallets: walletsResult[0],
+    duplicatedTransactions: duplicatedTransactionsResult
   };
 };
-
-
-// exports = async function(request, response){
-//   try {
-//     if (request.body === undefined) {
-//       response.setStatusCode(400);
-//       response.setBody(JSON.stringify({ "errorType": "MISSING_DATA" }));
-//       return;
-//     }
-//
-//     if (!context.user || !context.user.custom_data.roles.includes("seller")) {
-//       response.setStatusCode(401);
-//       response.setBody(JSON.stringify({ "errorType": "UNAUTHORIZED_ACCESS" }));
-//       return;
-//     }
-//
-//     const body = JSON.parse(await request.body.text());
-//
-//     if (body.start_date === undefined || body.end_date === undefined) {
-//       response.setStatusCode(400);
-//       response.setBody(JSON.stringify({ "errorType": "MISSING_DATA" }));
-//       return;
-//     }
-//
-//     const mongodb = context.services.get("mongodb-atlas");
-//
-//     const query = {
-//       timestamp: {
-//         $gte: body.start_date,
-//         $lt: body.end_date
-//       }
-//     };
-//
-//     const transactions = mongodb.db("clients").collection("transactions")
-//         .find({}).toArray();
-//
-//     let totalRevenue = 0;
-//
-//     for (let i = 0; i < transactions.length; i++) {
-//       totalRevenue += transactions[i].value;
-//     }
-//
-//     return { revenue: totalRevenue, transactions: transactions[0].value };
-//     response.setBody(JSON.stringify({ revenue: totalRevenue, transactions: transactions }));
-//   } catch (error) {
-//     response.setStatusCode(400);
-//     response.setBody(JSON.stringify({ "errorType": "ERROR", "message": error.message }));
-//   }
-// };
