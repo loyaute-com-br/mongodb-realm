46ab40e93b3d1c46056cce3d930133723a834de8
GuilhermeDSantos <53098377+GuilhermeDSantos@users.noreply.github.com>
2024-02-19 09:24:19 -0300

    Update getDashboard.js

--- functions/getDashboard.js
+++ functions/getDashboard.js
@@ -38,7 +38,7 @@ exports = async function(request, response){
       revenue += transactions.get(i).value;
     }
 
-    return { "revenue": revenue, "transactions": transactions };
+    return { "revenue": revenue, "transactions": transactions.get(0).value };
     // response.setBody(JSON.stringify({ "revenue": revenue, "body": body, "transactions": transactions }));
   } catch (error) {
     response.setStatusCode(400);
