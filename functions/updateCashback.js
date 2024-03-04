fd2315df5e67911dbe4457d22fa3184ca56aa746
GuilhermeDSantos <53098377+GuilhermeDSantos@users.noreply.github.com>
2024-03-04 10:39:43 -0300

    Update updateCashback.js

--- functions/updateCashback.js
+++ functions/updateCashback.js
@@ -1,38 +1,52 @@
-exports = async function() {
-  // Obtém a data atual sem o horário
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
+exports = async function () {
+    const session = mongodb.startSession();
 
-  // Acessa as coleções necessárias
-  const transactionsCollection = context.services.get("mongodb-atlas").db("clients").collection("transactions");
-  const walletsCollection = context.services.get("mongodb-atlas").db("clients").collection("wallets");
+    try {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
 
-  // Consulta as transações que correspondem aos critérios
-  const transactions = await transactionsCollection.find({
-    "cashback_availability": today,
-    "cashback_status": "NOT_EARNED"
-  }).toArray();
+        const transactionsCollection = context.services.get("mongodb-atlas").db("clients").collection("transactions");
+        const walletsCollection = context.services.get("mongodb-atlas").db("clients").collection("wallets");
 
-  // Para cada transação encontrada, atualiza a carteira correspondente e altera o status da transação
-  for (const transaction of transactions) {
-    // Encontra a carteira associada à transação
-    const wallet = await walletsCollection.findOne({ "_id": transaction.wallet_id });
+        const transactions = await transactionsCollection.find({
+            "cashback_availability": today,
+            "cashback_status": "NOT_EARNED"
+        }).toArray();
 
-    // Acrescenta o valor do cashback ganho ao saldo da carteira
-    const newBalance = wallet.balance + transaction.earned_cashback;
+        const transactionOptions = {
+            readPreference: "primary",
+            readConcern: {level: "local"},
+            writeConcern: {w: "majority"}
+        };
 
-    // Atualiza o saldo da carteira
-    await walletsCollection.updateOne(
-        { "_id": transaction.wallet_id },
-        { "$set": { "balance": newBalance } }
-    );
+        for (const transaction of transactions) {
+            await session.withTransaction(async () => {
+                const wallet = await walletsCollection.findOne({"_id": transaction.wallet_id});
 
-    // Atualiza o status da transação para "EARNED"
-    await transactionsCollection.updateOne(
-        { "_id": transaction._id },
-        { "$set": { "cashback_status": "EARNED" } }
-    );
-  }
+                const newBalance = wallet.balance + transaction.earned_cashback;
 
-  return "Operação concluída com sucesso";
+                await walletsCollection.updateOne(
+                    {"_id": transaction.wallet_id},
+                    {"$set": {"balance": newBalance}}
+                );
+
+                await transactionsCollection.updateOne(
+                    {"_id": transaction._id},
+                    {"$set": {"cashback_status": "EARNED"}}
+                );
+            }, transactionOptions);
+
+            await session.commitTransaction();
+        }
+    } catch (error) {
+        await session.abortTransaction();
+
+        response.setStatusCode(400);
+        response.setBody(JSON.stringify({"error": {"message": error.message}}));
+    } finally {
+        // Encerrar a sessão
+        session.endSession();
+    }
+
+    return "Operação concluída com sucesso";
 };
