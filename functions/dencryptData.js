2e9125adcd4f61f1fb062b47e349afe5731bd31e
GuilhermeDSantos <53098377+GuilhermeDSantos@users.noreply.github.com>
2024-03-04 13:27:21 -0300

    Update dencryptData.js

--- functions/dencryptData.js
+++ functions/dencryptData.js
@@ -2,10 +2,10 @@ exports = async function encryptData(data){
   const crypto = require('crypto');
 
   const key = 'banana';
-  
-  const cipher = crypto.createCipher('aes-256-cbc', key);
-  let encryptedData = cipher.update(data, 'utf8', 'hex');
-  encryptedData += cipher.final('hex');
-  
-  return encryptedData;
+
+  const decipher = crypto.createDecipher('aes-256-cbc', key);
+  let decryptedData = decipher.update(encryptedData, 'hex', 'utf8');
+  decryptedData += decipher.final('utf8');
+
+  return decryptedData;
 }
